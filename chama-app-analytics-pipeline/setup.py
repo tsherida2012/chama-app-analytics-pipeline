7c515bd18e3d5240bfbf6296aba5b78f09085410
Thomas Sheridan <tsherida2012@gmail.com>
2025-03-28 14:16:40 -0700

    project creation commit

---
+++ chama-app-analytics-pipeline/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='chama app analytics',
+    author='Thomas Sheridan',
+    license='MIT',
+)
